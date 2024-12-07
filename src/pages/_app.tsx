87dc413f90f4696918748e74a683d8a852438fe5
AJ Espinosa <espinosa.aaron45@gmail.com>
2024-12-07 23:51:56 +0800

    made responsive

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Rotating Crew Aus</title>
+        <meta
+          name="Rotating Crew Aus"
+          content="DefaultRotating Crew Aus delivers expert engineering solutions in oil and gas, mining, and construction with more than 16 years of experience. We specialize in innovative, efficient, and sustainable approaches across key industries. description for my Next.js app"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,21 @@ import { Rethink_Sans } from "next/font/google";
 import Image from "next/image";
 import { Mail, Phone } from "lucide-react";
 import { useEffect, useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 
 const rethink = Rethink_Sans({
   subsets: ["latin"],
@@ -21,7 +36,6 @@ export default function Home() {
     }
 
     window.addEventListener("scroll", handleScroll);
-    // Cleanup event listener on component unmount
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
@@ -29,12 +43,19 @@ export default function Home() {
   return (
     <div className={rethink.className}>
       <main className="relative h-full w-full font-semibold">
-        <div className="relative bg-[url('/Hero.webp')] bg-cover bg-top">
-          {/* <div className="absolute left-0 z-10 h-full w-full max-w-screen-2xl bg-gradient-to-l from-transparent to-blue-950" /> */}
+        <div className="relative">
+          <Image
+            src={"/Hero.webp"}
+            alt="hero image"
+            width="1000"
+            height="1000"
+            className="absolute top-0 h-full w-full object-cover object-top brightness-50 filter md:brightness-75 lg:brightness-90"
+          />
           <div className="absolute top-0 z-10 h-64 w-full bg-gradient-to-t from-transparent to-white" />
+
           {/* NAV BAR */}
           <nav
-            className={`sticky top-0 z-50 flex w-full items-center justify-between px-48 duration-300 ease-in-out ${scroll ? "bg-white py-3" : "bg-none py-6"}`}
+            className={`fixed top-0 z-50 flex w-full items-center justify-between px-10 duration-300 ease-in-out lg:px-24 xl:px-48 ${scroll ? "bg-white py-3" : "bg-none py-6"}`}
           >
             <div className="flex items-center gap-x-2">
               <Image
@@ -57,7 +78,7 @@ export default function Home() {
                 </p>
               </div>
             </div>
-            <div className="flex gap-x-10">
+            <div className="hidden gap-x-4 lg:flex xl:gap-x-10">
               {NAV_LINKS.map(({ name, link }) => (
                 <Link key={link} href={link}>
                   <p className="decoration-2 hover:font-bold hover:underline">
@@ -66,7 +87,7 @@ export default function Home() {
                 </Link>
               ))}
             </div>
-            <div className="flex flex-col">
+            <div className="hidden flex-col lg:flex">
               <div className="flex items-center gap-x-2">
                 <Mail size={20} />
                 <p>rotatingcrewaus@gmail.com</p>
@@ -79,8 +100,8 @@ export default function Home() {
           </nav>
 
           {/* HERO SECTION */}
-          <div className="relative z-30 flex flex-col gap-y-6 px-48 py-36">
-            <h1 className="max-w-2xl text-6xl font-black text-white drop-shadow-sm">
+          <div className="relative z-30 flex flex-col gap-y-6 px-10 pb-36 pt-48 lg:px-24 xl:px-48">
+            <h1 className="max-w-2xl text-5xl font-black text-white drop-shadow-sm lg:text-6xl">
               <span className="text-primary">Innovative</span> Engineering
               Solutions for a Sustainable{" "}
               <span className="text-primary">Future</span>
@@ -96,13 +117,23 @@ export default function Home() {
             </Link>
           </div>
         </div>
+
         {/* ABOUT SECTION */}
-        <div className="flex items-center gap-x-40 rounded-lg bg-white px-48 py-24">
+        <div className="flex items-center rounded-lg bg-white px-10 py-24 lg:gap-x-10 lg:px-24 xl:gap-x-40 xl:px-48">
           <div className="flex w-full flex-col gap-y-4">
-            <h1 className="max-w-2xl text-6xl font-black text-primary drop-shadow-sm">
+            <h1 className="max-w-2xl text-4xl font-black text-primary drop-shadow-sm lg:text-6xl">
               About us
             </h1>
-            <div className="flex flex-col gap-y-4">
+            <div className="h-60 w-full overflow-hidden drop-shadow-[-8px_8px_0px_#F11622] sm:h-96 lg:hidden">
+              <Image
+                src={"/Laser-Alignment-6.webp"}
+                alt="logo"
+                width="1000"
+                height="1000"
+                className="h-full w-full rounded-br-[3rem] object-cover object-center"
+              />
+            </div>
+            <div className="flex flex-col gap-y-4 py-4 lg:py-0">
               <p className="max-w-2xl text-blue-950 drop-shadow-md">
                 Rotating Crew Aus provides exceptional engineering consulting
                 services in the oil and gas, mining and construction industry
@@ -123,7 +154,7 @@ export default function Home() {
               </p>
             </div>
           </div>
-          <div className="w-full">
+          <div className="hidden w-full lg:block">
             <div className="h-[25rem] w-full overflow-hidden drop-shadow-[-8px_8px_0px_#F11622]">
               <Image
                 src={"/Laser-Alignment-6.webp"}
